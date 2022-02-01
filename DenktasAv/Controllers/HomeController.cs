fedcc227c191a1ad9032efe25ec764f5e1ff12be
İbrahim OKUYUCU <ibrahim.okuyucu@setup34.com.tr>
2022-02-01 10:22:40 +0300

    Add project files.

---
+++ DenktasAv/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DenktasAv.Controllers
+{
+    public class HomeController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("hakkimizda")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("iletisim")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("pinar-denktas")]
+        public ActionResult WriterProfile()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("yazarin-yazilari")]
+        public ActionResult WriterBlogs()
+        {            
+            return View();
+        }
+
+        [Route("uzmanlik-alanlari")]
+        public ActionResult AreasOfExpertise()
+        {
+            return View();
+        }
+
+        [Route("iletisim-formu")]
+        public ActionResult ContactForm()
+        {
+            return View();
+        }
+
+        [Route("blog")]
+        public ActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
